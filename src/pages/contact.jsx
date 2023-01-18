e2f6d875bb627a75e1dfd0ce12b66e3017f8b84e
Kaitlin Shumake <shumakek@ufl.edu>
2023-01-17 21:33:35 -0500

    started contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/contact.module.css'
+
+export default function Contact() {
+    return (
+        <main className={styles.contactmain}>
+            <div>
+                <h1>Contact Me Below:</h1>
+            </div>
+        </main>
+    )
+}
