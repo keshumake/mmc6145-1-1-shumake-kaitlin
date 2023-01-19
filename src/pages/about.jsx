bee261085c43c4a1b5cab63cc1b0adf1ba692cee
Kaitlin Shumake <shumakek@ufl.edu>
2023-01-18 21:21:09 -0500

    building home page

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/about.module.css'
+
+export default function About() {
+    return (
+        <main className={styles.aboutmain}>
+            <div>
+                <h1>Contact Me Below:</h1>
+            </div>
+        </main>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,25 @@
 import styles from '../styles/home.module.css'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
+    <Header />
     <main>
-      <h1 className={styles.placeholder}>I'm the home page!</h1>
+      <div>
+      <h1 className={styles.placeholder}>Kaitlin Shumake Portfolio</h1>
+      </div>
+      <div className='homebody'>
+        <section className={projectone}>
+          
+        </section>
+        <section className={projecttwo}>
+          
+        </section>
+        <section className={projectthree}>
+          
+        </section>
+      </div>
     </main>
   )
 }
