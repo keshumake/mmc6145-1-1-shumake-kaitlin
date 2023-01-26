3dc576862800d864386d1d34f103741c8bd26631
Kaitlin Shumake <shumakek@ufl.edu>
2023-01-26 15:36:48 -0500

    added components and children

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,7 +15,41 @@ export default function Home() {
       <div className={styles.placeholder}>
         <h1>Kaitlin Shumake Portfolio</h1>
       </div>
-      <Projects />
+      <Projects>
+      <div className={styles.projectcard}>
+      <h2>This project included a design for a local business.</h2>
+            <button>
+              <a href="https://stephaniemarzan.github.io/p1-wallshein-lewis-marzan-shumake/">
+                Project 1
+              </a>
+            </button>
+            </div>
+      </Projects>
+      <Projects>
+      <div className={styles.projectcard}>
+
+      <h2>
+              This project was completed as part of an assignment to create a
+              website for a non-profit organization.
+            </h2>
+            <button>
+              <a href="https://kjm2023.github.io/p2-shumake-lewis-cigarroa-mcleod/">
+                Project 2
+              </a>
+            </button>
+            </div>
+      </Projects>
+      <Projects>
+      <div className={styles.projectcard}>
+      <h2>This project was to design a website based on a movie.</h2>
+            <button>
+              <a href="https://keshumake.github.io/gw3_shumake_wallshein_bennington/">
+                Project 3
+              </a>
+            </button>
+            <h2></h2>
+            </div>
+      </Projects>
     </main>
   );
 }
