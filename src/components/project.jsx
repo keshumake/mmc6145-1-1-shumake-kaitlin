b85f3cca6218da972727beb45b1a932329058495
Kaitlin Shumake <shumakek@ufl.edu>
2023-01-28 20:46:45 -0500

    Added projects and project props

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,17 +1,16 @@
 import styles from '../styles/home.module.css'
 
-export default function Projects({ children }) {
+export default function Projects(props) {
     return(
         <div className={styles.homebody}>
-        <section className={styles.projectone}>
-           { children }
-        </section>
-        <section className={styles.projecttwo}>
-            { children }
-        </section>
-        <section className={styles.projectthree}>
-            { children }
-        </section>
+        <div className={styles.projectcard}>
+            <h2>{props.description}</h2>
+            <button>
+              <a href={props.link}>
+                {props.title}
+              </a>
+            </button>
+            </div>
         </div>
         );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,48 +8,44 @@ export default function Home() {
     <main>
       <div className={styles.namecard}>
         <div className={styles.greeting}>
-          <h2>Thank you for visiting my portfolio site. Please see the projects
-          linked below!</h2>
+          <h2>
+            Thank you for visiting my portfolio site. Please see the projects
+            linked below!
+          </h2>
         </div>
       </div>
       <div className={styles.placeholder}>
         <h1>Kaitlin Shumake Portfolio</h1>
       </div>
-      <Projects>
-      <div className={styles.projectcard}>
-      <h2>This project included a design for a local business.</h2>
-            <button>
-              <a href="https://stephaniemarzan.github.io/p1-wallshein-lewis-marzan-shumake/">
-                Project 1
-              </a>
-            </button>
-            </div>
-      </Projects>
-      <Projects>
-      <div className={styles.projectcard}>
-
-      <h2>
-              This project was completed as part of an assignment to create a
-              website for a non-profit organization.
-            </h2>
-            <button>
-              <a href="https://kjm2023.github.io/p2-shumake-lewis-cigarroa-mcleod/">
-                Project 2
-              </a>
-            </button>
-            </div>
-      </Projects>
-      <Projects>
-      <div className={styles.projectcard}>
-      <h2>This project was to design a website based on a movie.</h2>
-            <button>
-              <a href="https://keshumake.github.io/gw3_shumake_wallshein_bennington/">
-                Project 3
-              </a>
-            </button>
-            <h2></h2>
-            </div>
-      </Projects>
+      <div className={styles.homebody}>
+      <section>
+        <div className={styles.projectone}>
+        <div className={styles.projectcard}>
+        <Projects
+          title={"Project 1"}
+          description={"This project included a design for a local business."}
+          link={
+            "https://stephaniemarzan.github.io/p1-wallshein-lewis-marzan-shumake/"
+          }
+        />
+        </div>
+      </div>
+      </section>
+      <section>
+      <div className={styles.projecttwo}>
+        <div className={styles.projectcard}>
+          <Projects title={"Project 2"} description={"This project was completed as part of an assignment to create a website for a non-profit organization."} link={"https://kjm2023.github.io/p2-shumake-lewis-cigarroa-mcleod/"} />
+        </div>
+        </div>
+        </section>
+        <section>
+          <div className={styles.projectthree}>
+          <div className={styles.projectcard}>
+          <Projects title={"Project 3"} description={"This project was to design a website based on a movie."} link={"https://keshumake.github.io/gw3_shumake_wallshein_bennington/"} />
+        </div>
+        </div>
+        </section>
+        </div>
     </main>
   );
 }
